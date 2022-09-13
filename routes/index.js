bfca3e179e83be23207d09717345355d6a414a30
LaeekAhmed <laeek385@gmail.com>
2022-09-12 21:20:10 -0400

    log

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,7 @@ const { json } = require('body-parser')
 
 // ⚠️ server side caching ;
 console.log(process.env.REDISCLOUD_URL)
-const redisClient = redis.createClient({
-  url: process.env.REDIS_URL
-});
+const redisClient = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
 
 (async () => {
   await redisClient.connect();
